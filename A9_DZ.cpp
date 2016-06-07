f9550dca8283bffa477db701549bec2623ba440c
MikKud <mishel-kudinov@mail.ru>
2016-06-07 16:03:21 +0300

    Update A9_DZ.cpp

--- A9_DZ.cpp
+++ A9_DZ.cpp
@@ -1,229 +1,174 @@
-
-// homework.cpp : Defines the entry point for the console application.
-//
-
-
 #include "stdafx.h"
-#include <stdio.h>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <fstream>
-#include "classes.h"
-
+#include "ComFile.h"
+#include "ComDir.h"
 using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
-	
+	try {
+		/*cout << "Name of first directory is <d1> " << endl << "in <d1> there are <d1_2> directory and files <f1> and <f2> and <f3>" << endl;
+		cout << "In <d1_2> file <f3>"<<endl<<endl;*/
+		ComDir d1("d1");
+		ComDir d2("d2");
+		/*ComDir d1_2("d1_2");
+		ComDir d1_3("d1_3");
+		ComDir d2_1("d2_1");*/
+		ComFile f1("f1", 1);
+		ComFile f2("f2", 2);
+		/*ComFile f3("f3", 3);
+		ComFile f4("f3", 4);
+		d1.addFile(f1);
+		f1.setData(2);
+		f1.reName("f1_2");
+		d1.addFile(f1);
+		d1.addFile(f2);
+		d1.addFile(f3);
+		d1_2.addFile(f3);
+		d1_2.addFile(f4);
+		d1_2.addDir(d2_1);
+		d1.addDir(d1_2);
+		d1.addDir(d1_3);
+		cout << "after using seatch <f3> in <d1> direcroty and lower programm found ";
+		d1.search(d1, "f3");
+		cout << "files" << endl;
+		cout << "after using seatch <f1> in <d1> direcroty and lower programm found ";
+		d1.search(d1, "f1");
+		cout << "files" << endl;
+		cout << "checking d1.getname and f1.getdata" << endl;
+		cout << d1.getName() << endl;
+		cout << f1.getData() << endl;
 
-		cout << "enter numbers for needed commands" << endl;
-		cout << " 1 - Create and Add home to library " << endl;
-		cout << " 2 - Delete home from library" << endl;
-		cout << " 3 - Save data" << endl;
-		cout << " 4 - Switch on/off GKH in the house" << endl;
-		cout << " 5 - change Amount of people in the house" << endl;
-		cout << " 6 - bill for the month" << endl;
-		cout << " 7 - get data" << endl;
-		cout << " 8 - check fo rnot payed" << endl;
-		cout << " 9 - show houses using GKH" << endl;
-		cout << " 10 - EXIT" << endl;
+		cout << endl << endl << endl;
+		d1.ListDir(d1);
+		cout << endl << endl;
+		d1.LSR(d1);
+		cout << endl << endl << endl << endl;
 
-		Library L1;
-		enum comands { one = 1, two, three, four, five, six, seven, eight, nine, ten };
-		int ent = 0;
-		House H1;
 
-		while (ent != ten)
+			cout << endl << endl << endl;*/
+
+		string command = "start";
+		ComDir* Uptr = new ComDir;
+		ComDir* Stptr = new ComDir;
+		ComFile* Fileptr = new ComFile;
+		ComFile* NewFptr = new ComFile;
+		ComDir* NewDptr = new ComDir;
+		NewFptr = &f2;
+		NewDptr = &d2;
+		Uptr = &d1;
+		Stptr = &d1;
+		Fileptr = &f1;
+		cout << "Your first direktory is <d1>" << endl;
+		cout << "to come to d1 use command gotop" << endl;
+		cout << "These are coomands to use" << endl << "list - to see what is in the directory" << endl << "ListALL - to see what is lower the directory" << endl << "cd - to go through direktories";
+		cout << endl << "open - to use file" << endl << "write - to chande data on an opened file" << endl << "search - to see in what directories(that are lower or actually the using one) there are files with a needed name" << endl;
+		cout << "newfile - to create new file in the current directory" << endl << "newdir - to create new dir in the curreant directory" << endl << "dname - to change current dir name" << endl << "fname - to change current file name" << endl; ;
+		cout << "getdata - data of opened file" << endl<< "delfile - to delete file from current directory"<< endl;
+		while (command != "end")
 		{
-			cin >> ent;
-			switch (ent)
+			cin >> command;
+			
+			if (command == "delfile")
 			{
-			case one://create home
+				cout << "enter name of file"<<endl<<endl;
+				string name;
+				cin >> name;
+				Uptr->delFile(name);
+			}
+			if (command == "getdata")
 			{
-				cout << "Enter  House number  GKH  Amount of people  IF there is DOLG" << endl;
-				int HN;
-				bool gkh;
-				int ap;
-				int p;
-				cin >> HN >> gkh >> ap >> p;
-				H1(HN, gkh, ap, p);
-
-				L1.addHouse(H1);
-				{
-					string change;
-					change = "A new house was created with number " + to_string(HN) + " GKH is " + to_string(gkh) + "People amount is" + to_string(ap) + "Dolg is " + to_string(p);
-					L1.changes.push_back(change);
-				}
-				break;
-			}
-
-
-
-			case two://delete home
-			{
-				try {
-					cout << "Enter House Number to delete from library" << endl;
-					int N;
-					cin >> N;
-					L1.delHouse(N);
-					break;
-					{
-						string change;
-						change = "A House with number " + to_string(N) + "was destroyed";
-						L1.changes.push_back(change);
-					}
-				}
-				catch (invalid_argument& e)
-				{
-					cerr << e.what() << endl;
-					return -1;
-				}
-				break;
-			}
-			case three://save data
-			{
-				string S;
-				ofstream F;
-				cout << "write name of file " << endl;
-				cin >> S;
-				F.open(S);
-				F << "CountHomes " << L1.GetVector().size() << endl << endl;
-				for (int i = 0; i < L1.GetVector().size(); i++) {
-					F << L1.GetVector()[i] << endl;
-				}
-				F.close();
-				break;
-			}
-			case four://GKH
-			{
-				try {
-					cout << "Enter house number GKH to change and mode" << endl;
-					int N;
-					int m;
-					cin >> N >> m;
-					L1.changeGkh(N, m);
-					{
-						string change;
-						change = "A House with number " + to_string(N) + "Gkh was set to " + to_string(m);
-						L1.changes.push_back(change);
-					}
-				}
-				catch (invalid_argument& e)
-				{
-					cerr << e.what() << endl;
-					return -1;
-				}
-				break;
-			}
-			case five://change people
-			{try {
-				cout << "Enter house number People amount to change and the amount" << endl;
-				int N;
-				int pl;
-				cin >> N >> pl;
-				L1.changePpl(N, pl);
-				{
-					string change;
-					change = "A House with number " + to_string(N) + "amount of people was set to " + to_string(pl);
-					L1.changes.push_back(change);
-				}
-			}
-			catch (invalid_argument& e)
-			{
-				cerr << e.what() << endl;
-				return -1;
-			}
-				break;
-			}
-			case six://bill and if dolg
-			{try {
-				cout << "Enter house number bill to show and mode to change" << endl;
-				int N;
-				int p;
-				cin >> N >> p;
-				L1.showPrice(N, p);
-				{
-					string change;
-					change = "A House with number " + to_string(N) + "Payment dolg was set to " + to_string(p);
-					L1.changes.push_back(change);
-				}
-			}
-			catch (invalid_argument& e)
-			{
-				cerr << e.what() << endl;
-				return -1;
-			}
-				break;
-			}
-			case seven://get data
-			{
-				int HouseNumber;
-				bool GKH;
-				int people;
-				bool payed;
-				int G, N, p;
-				bool gkh, pay;
-				string buff;
-				string S;
-				ifstream F;
-				cout << "write name of file " << endl;
-				cin >> S;
-				F.open(S);
-				if (F.is_open()) {
-					F >> buff;
-					F >> G;
-					int i = 0;
-					while (i < G)
-					{
-						F >> buff;
-						F >> N;
-						F >> buff;
-						F >> gkh;
-						F >> buff;
-						F >> p;
-						F >> buff;
-						F >> pay;
-						H1(N, gkh, p, pay);
-						L1.addHouse(H1);
-						i++;
-						{
-							string change;
-							change = "A new house was created with number " + to_string(N) + " GKH is " + to_string(gkh) + "People amount is" + to_string(p) + "Dolg is " + to_string(pay);
-							L1.changes.push_back(change);
-						}
-					}
-					F.close();
-				}
-				break;
-			}
-			case eight://not payed
-			{
-				vector<int> dol;
-				dol = L1.getDolg();
-				for (int i = 0; i < dol.size(); i++)
-				{
-					cout << "House number " << dol[i] << " has Dolg ";
-				}
-				break;
-			}
-			case nine://show GKH houses
-			{
-				vector<int> usl;
-				usl = L1.showUslugi();
-				for (int i = 0; i < usl.size(); i++)
-				{
-					cout << "House number " << usl[i] << " using GKH ";
-				}
-			}
+				cout << Fileptr->getData() << endl << endl;
 
 			}
+
+			if (command == "dname")
+			{
+				cout << "enter name" << endl << endl;
+				string name;
+				cin >> name;
+				Uptr->setName(name);
+			}
+			if (command == "fame")
+			{
+				cout << "enter name" << endl << endl;
+				string name;
+				cin >> name;
+				Fileptr->reName(name);
+			}
+			if (command == "gotop")
+			{
+				Uptr = Stptr;
+				cout << "we now at the top directory" << endl << endl;
+			}
+			if (command == "newfile")
+			{
+				int data;
+				string name;
+				cout << "Enter name and data" << endl << endl;
+				cin >> name >> data;
+				NewFptr->reName(name);
+				NewFptr->setData(data);
+				Uptr->addFile(*NewFptr);
+				cout << "New File was created" << endl << endl;
+			}
+			if (command == "newdir")
+			{
+				string name;
+				cout << "Enter name" << endl << endl;
+				cin >> name;
+				NewDptr->setName(name);
+				Uptr->addDir(*NewDptr);
+				cout << "New Directory were created" << endl << endl;
+			}
+			if (command == "ListALL")
+			{
+				Uptr->LSR(*Uptr);
+			}
+			if (command == "list")
+			{
+				Uptr->ListAll();
+			}
+
+			if (command == "cd")
+			{
+				cout << "enter name of directory" << endl << endl;
+				ComDir* Tempptr = new ComDir;
+				Tempptr = (Uptr->cd());
+				Uptr = Tempptr;
+			}
+			if (command == "open")
+			{
+				cout << "enter name of file" << endl << endl;
+				Fileptr = Uptr->open();
+			}
+			if (command == "write")
+			{
+				cout << endl << "cin int " << endl << endl;
+				int k;
+				cin >> k;
+				Fileptr->setData(k);
+			}
+			if (command == "search")
+			{
+				string name;
+				cout << "cin name of file" << endl << endl;
+				cin >> name;
+				Uptr->search(*Uptr, name);
+			}
+			/*else
+			{
+				cout << endl << "wrong command" << endl;
+			}*/
 		}
-	
-	
-	cout << "Work is finished";
+	}
+	catch (invalid_argument& e)
+	{
+		cerr << e.what() << endl;
+		return -1;
+	}
 	system("pause");
-
 	return 0;
 }
-
